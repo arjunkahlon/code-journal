06492ec444134913b4387f57074cb5cf5ca6cac1
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-19 06:28:41 +0000

    Implemented local storage functionality in data.js. This includes stringifying and storing data on unload, as well as parsing JSON to data objects on page load. Functionality has been tested to ensure entries are properly saved to local storage.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntries = localStorage.getItem('entries-local-storage');
+if (previousEntries != null) {
+  data = JSON.parse(previousEntries);
+}
+
+window.addEventListener('beforeunload', storeEntries);
+
+function storeEntries() {
+  var saveEntries = JSON.stringify(data);
+  localStorage.setItem('entries-local-storage', saveEntries);
+}
