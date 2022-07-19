1836c2469809eae0b68bd9fe478724fbd3613102
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-19 00:53:23 +0000

    Completed submit event handler. The form input values and nextEntryId are placed into a new object. The object is prepended into the entries in the data model. nextEntryId is incremented in the data model. The image preview src attribute is reset. The form inputs are reset. Tested data model to ensure the proper storage of objects. Will implement local storage next.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,30 @@ var $photoURl = document.querySelector('#photo-url');
 var $photoPlaceholder = document.querySelector('#placeholder-image');
 $photoURl.addEventListener('input', handlePhotoInput);
 
+var $entryForm = document.querySelector('#journal-entry-form');
+$entryForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var journalEntry = {};
+  journalEntry.title = $entryForm.elements.title.value;
+  journalEntry.photoURL = $entryForm.elements.photoUrl.value;
+  journalEntry.notes = $entryForm.elements.notes.value;
+  journalEntry.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(journalEntry);
+  resetPlaceholder($photoPlaceholder);
+  $entryForm.reset();
+}
+
 function handlePhotoInput(event) {
   if (event.target.value) {
     $photoPlaceholder.setAttribute('src', $photoURl.value);
   } else {
-    $photoPlaceholder.setAttribute('src', './images/placeholder-image-square.jpg');
+    resetPlaceholder($photoPlaceholder);
   }
+}
+
+function resetPlaceholder(placeholder) {
+  placeholder.setAttribute('src', './images/placeholder-image-square.jpg');
 }
