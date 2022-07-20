0e0e826a8b8c11660c58d3462b40a82fb7146040
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-20 00:18:35 +0000

    Created a function named renderEntry which creates a DOM tree based on the dummy entry sample I created in my previous commit. The function taken in a single journal entry object as an argument. Next, I will implement the functionality for rendering all journal entries in my list when the DOMContentLoaded event is fired.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $photoURl.addEventListener('input', handlePhotoInput);
 
 var $entryForm = document.querySelector('#journal-entry-form');
 $entryForm.addEventListener('submit', handleSubmit);
+renderEntryList();
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -28,4 +29,53 @@ function handlePhotoInput(event) {
 
 function resetPlaceholder(placeholder) {
   placeholder.setAttribute('src', './images/placeholder-image-square.jpg');
+}
+
+function renderEntry(entry) {
+  var entryLi = document.createElement('li');
+  entryLi.setAttribute('class', 'list-entry');
+
+  var rootDiv = document.createElement('div');
+  rootDiv.setAttribute('class', 'row');
+  entryLi.appendChild(rootDiv);
+
+  var colHalf1 = document.createElement('div');
+  colHalf1.setAttribute('class', 'column-half');
+  rootDiv.appendChild(colHalf1);
+
+  var imgWrap = document.createElement('div');
+  imgWrap.setAttribute('class', 'image-container');
+  colHalf1.appendChild(imgWrap);
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photoURL);
+  imgWrap.appendChild(image);
+
+  var colHalf2 = document.createElement('div');
+  colHalf2.setAttribute('class', 'column-half');
+  rootDiv.appendChild(colHalf2);
+
+  var entryWrap = document.createElement('div');
+  entryWrap.setAttribute('class', 'entry-wrapper');
+  colHalf2.appendChild(entryWrap);
+
+  var entryTitle = document.createElement('h2');
+  entryTitle.setAttribute('class', 'font-open-sans text-bold font-size-x-large');
+  entryTitle.textContent = entry.title;
+  entryWrap.appendChild(entryTitle);
+
+  var entryNotes = document.createElement('p');
+  entryNotes.setAttribute('class', 'font-open-sans');
+  entryNotes.textContent = entry.notes;
+  entryWrap.appendChild(entryNotes);
+
+  return entryLi;
+}
+
+function renderEntryList() {
+  var $entryList = document.querySelector('.entries-list');
+  for (let i = 0; i < data.entries.length; i++) {
+    var createdEntry = renderEntry(data.entries[i]);
+    $entryList.appendChild(createdEntry);
+  }
 }
